f41da38714c07e62b2516a9c90ca4cfc66393ca3
nimesh13ad <nimesh13ad@gmail.com>
2022-09-28 12:31:38 -0700

    complete one whole txn

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let { Client } = require('spartan-gold');
-let StakeBlockchain = require('./blockchain.js');
+let StakeBlockchain = require('./blockchain');
 let { getWeightedRandom } = require('./rand');
 
 let identityCount = 0;
@@ -14,22 +14,87 @@ module.exports = class StakeClient extends Client {
         this.identity = identityCount;
         identityCount += 1;
 
-        this.on(StakeBlockchain.ELECT_LEADER, this.electLeader);
+        this.on(StakeBlockchain.ELECT_WINNER, this.electWinner);
+        this.on(StakeBlockchain.ANNOUNCE_BLOCK, this.receiveBlock);
     }
 
     /**
     * Starts listeners and begins mining.
     */
     initialize() {
-        this.currentBlock = StakeBlockchain.makeBlock(this.address, this.lastConfirmedBlock);
-        setTimeout(() => this.emit(StakeBlockchain.ELECT_LEADER), 1000);
+        this.currentBlock = StakeBlockchain.makeBlock(this.address, this.lastBlock);
+        setTimeout(() => this.emit(StakeBlockchain.ELECT_WINNER), 1000);
     }
 
-    electLeader() {
-        // TODO - random pick with weight
-        // coinbase reward
-        
-        const winner = getWeightedRandom(this.lastConfirmedBlock);
-        
+    electWinner() {
+
+        this.currentBlock.winner = getWeightedRandom(this.currentBlock);
+        if (this.currentBlock.winner === this.address) {
+            this.announceProof();
+        }
+    }
+
+    announceProof() {
+        this.net.broadcast(StakeBlockchain.ANNOUNCE_BLOCK, this.currentBlock);
+    }
+
+    receiveBlock(block) {
+
+        block = StakeBlockchain.deserializeBlock(block);
+        let currentBlock = this.currentBlock;
+
+        if (currentBlock.winner === block.winner) {
+
+            // Storing the block.
+
+            // Make sure that we have the previous blocks, unless it is the genesis block.
+            // If we don't have the previous blocks, request the missing blocks and exit.
+            let prevBlock = this.blocks.get(block.prevBlockHash);
+            if (!prevBlock && !block.isGenesisBlock()) {
+                let stuckBlocks = this.pendingBlocks.get(block.prevBlockHash);
+
+                // If this is the first time that we have identified this block as missing,
+                // send out a request for the block.
+                if (stuckBlocks === undefined) {
+                    this.requestMissingBlock(block);
+                    stuckBlocks = new Set();
+                }
+                stuckBlocks.add(block);
+
+                this.pendingBlocks.set(block.prevBlockHash, stuckBlocks);
+                return null;
+            }
+
+            if (!block.isGenesisBlock()) {
+                // Verify the block, and store it if everything looks good.
+                // This code will trigger an exception if there are any invalid transactions.
+                let success = block.rerun(prevBlock);
+                if (!success) return null;
+            }
+
+            this.blocks.set(block.id, block);
+
+            // If it is a better block than the client currently has, set that
+            // as the new currentBlock, and update the lastConfirmedBlock.
+            if (this.lastBlock.chainLength < block.chainLength) {
+                this.lastBlock = block;
+                this.setLastConfirmed();
+            }
+
+            this.initialize();
+        }
+    }
+
+    setLastConfirmed() {
+        let block = this.lastBlock;
+
+        let confirmedBlockHeight = block.chainLength - StakeBlockchain.CONFIRMED_DEPTH;
+        if (confirmedBlockHeight < 0) {
+            confirmedBlockHeight = 0;
+        }
+        while (block.chainLength > confirmedBlockHeight) {
+            block = this.blocks.get(block.prevBlockHash);
+        }
+        this.lastConfirmedBlock = block;
     }
 }
