54462abc3c0ea1e46e7f1e12f532a6b144d8b80d
nimesh13ad <nimesh13ad@gmail.com>
2023-03-09 19:15:17 -0800

    BA and binary BA star stage 1

--- client.js
+++ client.js
@@ -40,6 +40,7 @@ module.exports = class StakeClient extends Client {
         this.currentBlock = StakeBlockchain.makeBlock(this.address, this.lastBlock);
         let seed = "seed";
         this.ctx = this.currentBlock.getContext(seed);
+        this.hblockStar = null;
 
         setTimeout(() => this.emit(StakeBlockchain.PROPOSE_BLOCK), 1000);
     }
@@ -107,7 +108,7 @@ module.exports = class StakeClient extends Client {
             }
         }
 
-        setTimeout(() => this.baStar(
+        setTimeout(() => this.reductionOne(
             this.currentBlock.chainLength,
             winningBlockhash
         ),
@@ -395,11 +396,6 @@ module.exports = class StakeClient extends Client {
         }
     }
 
-    // TODO: binary BA star algorithm to finish the consensus.
-    binaryBAStar(round, hblock) {
-        return null;
-    }
-
     receiveVote(vote) {
         let { voter, round, step } = vote;
         console.log(this.name, "Received vote from: ", voter);
@@ -412,5 +408,65 @@ module.exports = class StakeClient extends Client {
         }
 
         this.incomingMsgs.get(round).get(step).push(vote);
+    }
+
+    receiveBlock1(block) {
+
+        block = StakeBlockchain.deserializeBlock(block);
+        let currentBlock = this.currentBlock;
+
+        if (currentBlock.winner === block.winner) {
+
+            // Storing the block.
+
+            // Make sure that we have the previous blocks, unless it is the genesis block.
+            // If we don't have the previous blocks, request the missing blocks and exit.
+            let prevBlock = this.blocks.get(block.prevBlockHash);
+            if (!prevBlock && !block.isGenesisBlock()) {
+                let stuckBlocks = this.pendingBlocks.get(block.prevBlockHash);
+
+                // If this is the first time that we have identified this block as missing,
+                // send out a request for the block.
+                if (stuckBlocks === undefined) {
+                    this.requestMissingBlock(block);
+                    stuckBlocks = new Set();
+                }
+                stuckBlocks.add(block);
+
+                this.pendingBlocks.set(block.prevBlockHash, stuckBlocks);
+                return null;
+            }
+
+            if (!block.isGenesisBlock()) {
+                // Verify the block, and store it if everything looks good.
+                // This code will trigger an exception if there are any invalid transactions.
+                let success = block.rerun(prevBlock);
+                if (!success) return null;
+            }
+
+            this.blocks.set(block.id, block);
+
+            // If it is a better block than the client currently has, set that
+            // as the new currentBlock, and update the lastConfirmedBlock.
+            if (this.lastBlock.chainLength < block.chainLength) {
+                this.lastBlock = block;
+                this.setLastConfirmed();
+            }
+
+            this.initialize();
+        }
+    }
+
+    setLastConfirmed() {
+        let block = this.lastBlock;
+
+        let confirmedBlockHeight = block.chainLength - StakeBlockchain.CONFIRMED_DEPTH;
+        if (confirmedBlockHeight < 0) {
+            confirmedBlockHeight = 0;
+        }
+        while (block.chainLength > confirmedBlockHeight) {
+            block = this.blocks.get(block.prevBlockHash);
+        }
+        this.lastConfirmedBlock = block;
     }
 }
